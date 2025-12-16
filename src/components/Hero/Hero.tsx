d726fd98718c91e526ae2b5d00e23fd8c8724895
Juan David Franco <jfrancoh5@gmail.com>
2025-12-16 01:19:23 -0500

    media queries avances home

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -5,17 +5,19 @@ import ImageHero from "../../elements/ImageHero/ImageHero"
 export default function Hero():React.ReactNode {
   return (
     <section className="h-full w-full bg-white-font">
-      <div className="w-[95%] mx-auto">
-        <article className="flex w-11/12 mx-auto max-w-286">
-          <div className="w-183 h-156 grid grid-cols-[90px_127px_96px_76px_146px_1fr] grid-rows-[169px_89px_149px_1fr]">
+      <div className="w-[95%] mx-auto flex">
+        <article className="flex w-11/12 mx-auto max-w-286 max-tablet-hor:w-full max-tablet-hor:mx-0">
+          <div className="w-183 h-156 grid grid-cols-[90px_127px_96px_76px_146px_1fr] grid-rows-[169px_89px_149px_1fr] 
+          max-tablet-hor:grid-cols-[68px_195px_118px_1fr] max-tablet-hor:w-130">
             {imageHeroList.map((book)=>(
               <ImageHero src={book.src} className={book.className} alt={book.alt}/>
             ))}
-            <h1 className="text-font-big-64 text-tittle-blue font-semibold col-start-2 col-end-5 row-start-2 row-end-3 justify-self-end">Biblioteams</h1>
-            <p className="text-font-big-52 font-medium col-start-2 col-end-7 row-start-3 row-end-4">Un espacio de formación para niños y jóvenes</p>
+            <h1 className="text-font-big-64 text-tittle-blue font-semibold col-start-2 col-end-5 row-start-2 row-end-3 justify-self-end max-tablet-hor:col-start-1 max-tablet-hor:col-end-3 max-tablet-hor:justify-self-start 
+            max-tablet-hor:text-font-big-40 max-tablet-hor:self-end">Biblioteams</h1>
+            <p className="text-font-big-52 font-medium col-start-2 col-end-7 row-start-3 row-end-4 max-tablet-hor:col-start-1 max-tablet-hor:col-end-4 max-tablet-hor:text-font-h1-32">Un espacio de formación para niños y jóvenes</p>
           </div>
-          <figure className="w-125 h-125">
-            <img src="/src/assets/images/hero/libro-hero.png" alt="Libro Hero" className="w-full h-full" />
+          <figure className="w-125 h-125 max-tablet-hor:w-102.5 max-tablet-hor:h-102.5 max-tablet-hor:self-center ">
+            <img src="/src/assets/images/hero/libro-hero.png" alt="Libro Hero" className="w-full h-full max-tablet-hor:" />
           </figure>
         </article>
       </div>
--- src/elements/ImageHero/data.ts
+++ src/elements/ImageHero/data.ts
@@ -3,33 +3,32 @@ import type { ImageHeroProps } from "./types"
 export const imageHeroList:ImageHeroProps[] =[
     {
       src:"/src/assets/images/hero/iconos-hero.png",
-      ///public/images/hero/iconos-hero.png
-      className:"w-24 h-24 col-start-1 col-end-4 row-start-1 row-end-2 justify-self-end self-end",
+      className:"w-24 h-24 col-start-1 col-end-4 row-start-1 row-end-2 justify-self-end self-end max-tablet-hor:col-start-2 max-tablet-hor:col-end-3 max-tablet-hor:justify-self-center",
       alt:"biblioteca catálogo digital"
     },
     {
       src:"/src/assets/images/hero/iconos-hero-1.png",
-      className:"w-24 h-24 col-start-4 col-end-5 row-start-1 row-end-2 justify-self-end self-center",
+      className:"w-24 h-24 col-start-4 col-end-5 row-start-1 row-end-2 justify-self-end self-center max-tablet-hor:col-start-3 max-tablet-hor:col-end-4",
       alt:"biblioteca catálogo digital"
     },
     {
       src:"/src/assets/images/hero/iconos-hero-2.png",
-      className:"w-24 h-24 col-start-5 col-end-6 row-start-1 row-end-2 justify-self-end self-end",
+      className:"w-24 h-24 col-start-5 col-end-6 row-start-1 row-end-2 justify-self-end self-end max-tablet-hor:col-start-4 max-tablet-hor:col-end-5",
       alt:"biblioteca catálogo digital"
     },
     {
       src:"/src/assets/images/hero/iconos-hero-3.png",
-      className:"w-24 h-24 col-start-3 col-end-4 row-start-4 row-end-5",
+      className:"w-24 h-24 col-start-3 col-end-4 row-start-4 row-end-5 max-tablet-hor:col-start-2 max-tablet-hor:col-end-3 max-tablet-hor:self-center",
       alt:"biblioteca catálogo digital"
     },
     {
       src:"/src/assets/images/hero/iconos-hero-4.png",
-      className:"w-24 h-24 col-start-5 col-end-6 row-start-4 row-end-5 self-center",
+      className:"w-24 h-24 col-start-5 col-end-6 row-start-4 row-end-5 self-center max-tablet-hor:col-start-3 max-tablet-hor:col-end-4 max-tablet-hor:mt-20",
       alt:"biblioteca catálogo digital"
     },
     {
       src:"/src/assets/images/hero/iconos-hero-5.png",
-      className:"w-24 h-24 col-start-6 col-end-7 row-start-4 row-end-5 self-center",
+      className:"w-24 h-24 col-start-6 col-end-7 row-start-4 row-end-5 self-center max-tablet-hor:col-start-4 max-tablet-hor:col-end-5",
       alt:"biblioteca catálogo digital"
     }
   ]
